11bce69c363d76d66524a1aba11c3e7c69bbef1b
ThrasyvoulosK <69692688+ThrasyvoulosK@users.noreply.github.com>
2022-08-27 23:11:32 +0300

    CardDisplay cleanup and minor fixes

--- Assets/CardDiplay.cs
+++ Assets/CardDiplay.cs
@@ -29,39 +29,15 @@ public class CardDiplay : MonoBehaviour
             Debug.Log(card.name);
             Root root = Root.GetData("xy1-1");
 
-            SpriteRenderer spriteRenderer = card.gameObject.GetComponent<Image>().GetComponent<SpriteRenderer>();
-
-            //card.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = root.data.images.symbol;
-            /*Debug.Log(root.data.id);
-            Debug.Log(root.data.name);
-            Debug.Log(root.data.hp);
-            Debug.Log(root.data.images.logo);
-            */
-
-            //spriteRenderer.sprite= root.data.images.small;
-
-            //UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(root.data.images.small);
-            //spriteRenderer.sprite = Sprite.Create(DownloadHandlerTexture.GetContent(uwr), new Rect(0, 0, 50,50), new Vector2(0, 0));
-
-            StartCoroutine(GetText(card.gameObject,spriteRenderer));
-
-            //spriteRenderer.sprite = Resources.Load<Sprite>("https://images.pokemontcg.io/xy1/logo.png");
-
-            //card.gameObject.GetComponent<UnityEngine.UI.Image>().sprite= Resources.Load<Sprite>("https://images.pokemontcg.io/xy1/logo.png");
-            //Debug.Log(card.gameObject.GetComponent<UnityEngine.UI.Image>().sprite.name);
-
-            //Image image = card.gameObject.GetComponent<Image>();
-            //image.sprite= Resources.Load<Sprite>("https://images.pokemontcg.io/xy1/logo.png");
-            //spriteRenderer.sprite= Resources.Load<Sprite>("https://images.pokemontcg.io/xy1/logo.png");
+            StartCoroutine(GetText(card.gameObject,root));
         }
     }
 
-    IEnumerator GetText(GameObject gameObject,SpriteRenderer spriteRenderer)
+    IEnumerator GetText(GameObject gameObject,Root root)
     {
-        UnityWebRequest uwr = UnityWebRequestTexture.GetTexture("https://www.my-server.com/myimage.png");
+        //UnityWebRequest uwr = UnityWebRequestTexture.GetTexture("https://www.my-server.com/myimage.png");
+        UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(root.data.images.small);
         yield return uwr.SendWebRequest();
-        //spriteRenderer.sprite = Sprite.Create(DownloadHandlerTexture.GetContent(uwr), new Rect(0, 0, 50, 50), new Vector2(0, 0));
-        //Debug.Log(spriteRenderer.sprite.name);
 
         if (uwr.result != UnityWebRequest.Result.Success)
         {
@@ -70,30 +46,15 @@ public class CardDiplay : MonoBehaviour
         else
         {
             // Get downloaded asset bundle
-            //var texture = DownloadHandlerTexture.GetContent(uwr);
             //Debug.Log(texture.width);
             Texture2D webTexture = ((DownloadHandlerTexture)uwr.downloadHandler).texture as Texture2D;
             Sprite webSprite = SpriteFromTexture2D(webTexture);
             gameObject.GetComponent<Image>().sprite = webSprite;
+
+            gameObject.GetComponent<Image>().preserveAspect = true;
         }
 
-        /*using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture("https://www.my-server.com/myimage.png"))
-        {
-            yield return uwr.SendWebRequest();
-
-            if (uwr.result != UnityWebRequest.Result.Success)
-            {
-                Debug.Log(uwr.error);
-            }
-            else
-            {
-                // Get downloaded asset bundle
-                var texture = DownloadHandlerTexture.GetContent(uwr);
-                Debug.Log(texture.width);
-            }
-            spriteRenderer.sprite = Sprite.Create(DownloadHandlerTexture.GetContent(uwr), new Rect(0, 0, 50, 50), new Vector2(0, 0));
-            Debug.Log(spriteRenderer.sprite.name);
-        }*/
+        
     }
 
     Sprite SpriteFromTexture2D(Texture2D texture)
