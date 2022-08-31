7be2ec80831c6ba9bdeb09243d3270e82f3df567
ThrasyvoulosK <69692688+ThrasyvoulosK@users.noreply.github.com>
2022-08-31 04:28:49 +0300

    SortByHP Works Properly

--- Assets/DeckCardHandler.cs
+++ Assets/DeckCardHandler.cs
@@ -131,6 +131,26 @@ public class DeckCardHandler : MonoBehaviour //,IPointerClickHandler
 
     public void OrderDeckByHP()
     {
+        cardObjects.Sort((p1, p2) => (p1.GetComponent<CardStatsCondensed>().HP).CompareTo(p2.GetComponent<CardStatsCondensed>().HP));
+        int counter = 1;
+        //RectTransform rectTransform = new RectTransform;
+        //rectTransform = gameObject.transform.GetChild(1).position;
+        //keep original position to rebuild later
+        Vector2 vector2 = gameObject.transform.GetChild(1).GetComponent<RectTransform>().anchoredPosition;
+        //Debug.Log(Vector2.y.ToString())
+        foreach (GameObject gameObject in cardObjects)
+        {
+            Debug.Log("Clone" + gameObject.name);
+            Debug.Log("Clone" + gameObject.GetComponent<CardStatsCondensed>().HP);
+            Debug.Log("Clone" + gameObject.GetComponent<CardStatsCondensed>().cardName);
+            //change position between siblings in transform
+            gameObject.transform.SetSiblingIndex(counter);
+            gameObject.GetComponent<RectTransform>().anchoredPosition= new Vector2(0, vector2.y - 15f*(counter-1));
+            counter++;
+        }
+    }
+    public void OrderDeckByHP2()
+    {
         //create a clone of the original list
         List<GameObject> cardObjectsClone = new List<GameObject>();
         foreach (GameObject gameObject in cardObjects)
@@ -142,11 +162,12 @@ public class DeckCardHandler : MonoBehaviour //,IPointerClickHandler
         }
         //sort clone
         //cardObjectsClone.Sort(SortByHP);
-        cardObjectsClone.Sort((p1, p2) => p1.GetComponent<CardStatsCondensed>().HP.CompareTo(p2.GetComponent<CardStatsCondensed>().HP));
+        cardObjectsClone.Sort((p1, p2) => (p1.GetComponent<CardStatsCondensed>().HP).CompareTo(p2.GetComponent<CardStatsCondensed>().HP));
         foreach (GameObject gameObject in cardObjectsClone)
         {
             Debug.Log("Clone"+gameObject.name);
             Debug.Log("Clone"+gameObject.GetComponent<CardStatsCondensed>().HP);
+            Debug.Log("Clone"+gameObject.GetComponent<CardStatsCondensed>().cardName);
             
         }
 
@@ -160,10 +181,10 @@ public class DeckCardHandler : MonoBehaviour //,IPointerClickHandler
             //GameObject newGameObject=new GameObject();
             //newGameObject= Instatiate(cardObjectsClone[i]);
             //cardObjectsClone[i].name.Remove(-6);//remove clone from name
-            cardObjectsClone[i].name = cardObjectsClone[i].name.Substring(0,cardObjectsClone[i].name.Length- 7);
+            //cardObjectsClone[i].name = cardObjectsClone[i].name.Substring(0,cardObjectsClone[i].name.Length- 7);
             Debug.Log(cardObjectsClone[i].name);
             cardObjects.Add(cardObjectsClone[i]);
-            cardObjects[i].GetComponent<TextMeshProUGUI>().SetText(cardObjectsClone[i].name);
+            cardObjects[i].GetComponent<TextMeshProUGUI>().SetText(cardObjectsClone[i].name.Substring(0, cardObjectsClone[i].name.Length - 7));
         }
         
         /*for (int i=0;i<cardObjects.Count;i++)
